ab2a3a5d760407a94c9733b96c31cf39123296da
Christophe Muller <mullerchristophe21@gmail.com>
2026-08-16 22:17:50 +0100

    update data

--- src/data/committeeData.js
+++ src/data/committeeData.js
@@ -1,20 +1,20 @@
 export const COMMITTEE_MEMBERS = [
   {
-    id: 'rhys-inward',
-    name: 'Rhys Inward',
+    id: 'josh-steele',
+    name: 'Josh Steele',
     role: 'President',
     email: 'mcr.president@jesus.ox.ac.uk',
     image: 'https://static.wixstatic.com/media/78866e_0b461e601b864ea6b9e02c82d1b99509~mv2.png/v1/crop/x_139,y_0,w_1211,h_1597/fill/w_546,h_720,al_c,q_90,enc_avif,quality_auto/empty_edited.png',
-    bio: 'I’m currently in the second year of my DPhil, focusing on genomic epidemiology and emerging infectious diseases (feel free to chat with me about it anytime!). Over the past year and a half, I’ve served as your Social Secretary, which is where many of you may already know me from. You might also catch me singing in the chapel on Sundays or around college, please don’t hesitate to say hi.',
+    bio: 'tbd',
     category: 'exec'
   },
   {
-    id: 'fergus-kirman',
-    name: 'Fergus Kirman',
+    id: 'christophe-muller',
+    name: 'Christophe Muller',
     role: 'Vice President',
     email: 'mcr.vice-president@jesus.ox.ac.uk',
-    image: 'https://static.wixstatic.com/media/78866e_a658c32347104be4970b544b323511b0~mv2.jpg/v1/crop/x_0,y_7,w_1536,h_2025/fill/w_546,h_720,al_c,q_85,enc_avif,quality_auto/fergus.jpg',
-    bio: 'Hello! My name is Fergus and I’m currently studying for a Master’s in Comparative Social Policy.',
+    image: 'https://static.wixstatic.com/media/78866e_0b461e601b864ea6b9e02c82d1b99509~mv2.png/v1/crop/x_139,y_0,w_1211,h_1597/fill/w_546,h_720,al_c,q_90,enc_avif,quality_auto/empty_edited.png',
+    bio: 'tbd',
     category: 'exec'
   },
   {
@@ -27,66 +27,66 @@ export const COMMITTEE_MEMBERS = [
     category: 'exec'
   },
   {
-    id: 'benjamin-micallef',
-    name: 'Benjamin Micallef',
+    id: 'secretary',
+    name: 'Missing',
     role: 'Secretary',
     email: 'mcr.secretary@jesus.ox.ac.uk',
     image: 'https://static.wixstatic.com/media/78866e_0b461e601b864ea6b9e02c82d1b99509~mv2.png/v1/crop/x_139,y_0,w_1211,h_1597/fill/w_546,h_720,al_c,q_90,enc_avif,quality_auto/empty_edited.png',
-    bio: 'Organises General Meetings, drafts and publishes meeting minutes, maintains the MCR Constitution, and handles internal communications and membership records.',
+    bio: 'tbd',
     category: 'exec'
   },
   {
-    id: 'zephyr-verwimp',
-    name: 'Zephyr Verwimp',
+    id: 'social-secretary-1',
+    name: 'Missing',
     role: 'Social Secretary',
     email: 'jesus_mcr_social@maillist.ox.ac.uk',
     image: 'https://static.wixstatic.com/media/78866e_0b461e601b864ea6b9e02c82d1b99509~mv2.png/v1/crop/x_139,y_0,w_1211,h_1597/fill/w_546,h_720,al_c,q_90,enc_avif,quality_auto/empty_edited.png',
-    bio: 'Hello! I am Zephyr, a first-year DPhil student in Computer Science. With the other social secretaries, I organise social events for the MCR like BOPs, exchange dinners, wine & cheese nights, and more. Feel free to reach out if you want to organise a social event in college, or even just to have a chat.',
+    bio: 'tbd',
     category: 'social'
   },
   {
-    id: 'erica-yang',
-    name: 'Erica Yang',
+    id: 'social-secretary-2',
+    name: 'Missing',
     role: 'Social Secretary',
     email: 'jesus_mcr_social@maillist.ox.ac.uk',
-    image: 'https://static.wixstatic.com/media/78866e_3f92c05ec9c14555b87c01767f322cce~mv2.jpg/v1/crop/x_0,y_194,w_2136,h_2817/fill/w_546,h_720,al_c,q_85,enc_avif,quality_auto/erica.jpg',
-    bio: 'Hi, I’m Erica! I’m doing my master’s in Sociology, and I’m one of the social secretaries at Jesus College. We organise various social events for MCR members, such as Wine & Cheese, BOPs, exchange formals and many more. I love socialising and connecting with others – whether you’re part of our college or beyond, we’re always excited to collaborate and get to know more people. Social events are key to creating a welcoming and vibrant environment, and we hope to bring the best experience to the community. Please feel free to reach out :)',
+    image: 'https://static.wixstatic.com/media/78866e_0b461e601b864ea6b9e02c82d1b99509~mv2.png/v1/crop/x_139,y_0,w_1211,h_1597/fill/w_546,h_720,al_c,q_90,enc_avif,quality_auto/empty_edited.png',
+    bio: 'tbd',
     category: 'social'
   },
   {
-    id: 'guste-gurcinaite',
-    name: 'Gustė Gurčinaitė',
+    id: 'environmental-officer',
+    name: 'Missing',
     role: 'Environmental Officer',
     email: 'mcr.environmental-rep@jesus.ox.ac.uk',
-    image: 'https://static.wixstatic.com/media/78866e_0c3ec8e055fa4f4bb2f89adf91d2afbe~mv2.jpg/v1/crop/x_247,y_0,w_2242,h_2956/fill/w_546,h_720,al_c,q_85,enc_avif,quality_auto/guste.jpg',
-    bio: 'Gustė is a first-year DPhil student in Geography and the Environment, with her research focusing on just energy transition policy in the European Union. Originally from Lithuania, she has lived across three continents, with Oxford being the most recent addition to the many places she calls home. As an Environmental Officer, Gustė promotes sustainability through informal events, supports college net-zero targets, and discusses the ethical investment policy.',
+    image: 'https://static.wixstatic.com/media/78866e_0b461e601b864ea6b9e02c82d1b99509~mv2.png/v1/crop/x_139,y_0,w_1211,h_1597/fill/w_546,h_720,al_c,q_90,enc_avif,quality_auto/empty_edited.png',
+    bio: 'tbd',
     category: 'reps'
   },
   {
-    id: 'michael-beukman',
-    name: 'Michael Beukman',
+    id: 'computing-officer',
+    name: 'Missing',
     role: 'Computing Officer',
     email: 'mcr.computing@jesus.ox.ac.uk',
-    image: 'https://static.wixstatic.com/media/78866e_497f8927a9914e9494af277ce9d2e119~mv2.jpg/v1/crop/x_0,y_47,w_1649,h_2160/fill/w_546,h_720,al_c,q_85,enc_avif,quality_auto/mike_edited.jpg',
-    bio: 'I’m Michael, a first-year DPhil student in Machine Learning. I am responsible for all IT-related MCR activities, such as this website! If you have IT queries, or website suggestions, please reach out!',
+    image: 'https://static.wixstatic.com/media/78866e_0b461e601b864ea6b9e02c82d1b99509~mv2.png/v1/crop/x_139,y_0,w_1211,h_1597/fill/w_546,h_720,al_c,q_90,enc_avif,quality_auto/empty_edited.png',
+    bio: 'tbd',
     category: 'operations'
   },
   {
-    id: 'mahnoor-shoaib',
-    name: 'Mahnoor Shoaib',
+    id: 'edi-officer',
+    name: 'Missing',
     role: 'Equality & Diversity Officer',
     email: 'mcr.equalities@jesus.ox.ac.uk',
-    image: 'https://static.wixstatic.com/media/78866e_5ef24f1b7d524d549ff155c8013fd3de~mv2.jpeg/v1/crop/x_264,y_0,w_2208,h_2911/fill/w_546,h_720,al_c,q_85,enc_avif,quality_auto/Mahnoor.jpeg',
-    bio: 'Hi, my name is Mahnoor Shoaib and I am your EDI Officer! As part of my role, I attend EDI workshops and meetings over the term and feedback any updates during the MCR General Meetings. Events include brunches, hikes, and day trips. I look forward to getting to know you all!',
+    image: 'https://static.wixstatic.com/media/78866e_0b461e601b864ea6b9e02c82d1b99509~mv2.png/v1/crop/x_139,y_0,w_1211,h_1597/fill/w_546,h_720,al_c,q_90,enc_avif,quality_auto/empty_edited.png',
+    bio: 'tbd',
     category: 'reps'
   },
   {
-    id: 'sophie-marlow',
-    name: 'Sophie Marlow',
+    id: 'sophie-cook',
+    name: 'Sophie Cook',
     role: 'Arts Representative',
     email: 'mcr.arts@jesus.ox.ac.uk',
-    image: 'https://static.wixstatic.com/media/78866e_6d8cf61026884ab8b590fa389a940bf1~mv2.jpeg/v1/crop/x_0,y_22,w_3000,h_3956/fill/w_546,h_720,al_c,q_85,enc_avif,quality_auto/sophie.jpeg',
-    bio: 'Hi I’m Sophie, a DPhil student in Cardiovascular Science and one of your MCR arts reps. Get in contact if you have any craft ideas you’d like to be organised as an event this term!',
+    image: 'https://static.wixstatic.com/media/78866e_0b461e601b864ea6b9e02c82d1b99509~mv2.png/v1/crop/x_139,y_0,w_1211,h_1597/fill/w_546,h_720,al_c,q_90,enc_avif,quality_auto/empty_edited.png',
+    bio: 'tbd',
     category: 'reps'
   },
   {
@@ -94,17 +94,17 @@ export const COMMITTEE_MEMBERS = [
     name: 'Kristen McCollum',
     role: 'Mature Students Representative',
     email: 'jesus_mcr_mature_student_rep@maillist.ox.ac.uk',
-    image: 'https://static.wixstatic.com/media/78866e_7f4a7a208c2843d1b15669a1cfad23f9~mv2.jpeg/v1/crop/x_0,y_22,w_3024,h_3988/fill/w_546,h_720,al_c,q_85,enc_avif,quality_auto/kristen.jpeg',
+    image: 'https://static.wixstatic.com/media/78866e_0b461e601b864ea6b9e02c82d1b99509~mv2.png/v1/crop/x_139,y_0,w_1211,h_1597/fill/w_546,h_720,al_c,q_90,enc_avif,quality_auto/empty_edited.png',
     bio: 'I’m Kristen, a DPhil student in Migration Studies. I started my DPhil after several years on my career track, and I’m looking forward to supporting other students who are embarking on their postgraduate studies at a later stage in life. If you are looking for a community of peers or have concerns about integrating with college life, let’s grab a coffee and chat.',
     category: 'reps'
   },
   {
-    id: 'oliver-smith',
-    name: 'Oliver Smith',
+    id: 'rhys-inward',
+    name: 'Rhys Inward',
     role: 'Sports Representative',
     email: 'mcr.sports-rep@jesus.ox.ac.uk',
-    image: 'https://static.wixstatic.com/media/78866e_d773fe2d2e924b8b9b18db4cb1e3b66c~mv2.jpg/v1/crop/x_1118,y_0,w_2948,h_3888/fill/w_546,h_720,al_c,q_85,enc_avif,quality_auto/oliver_JPG.jpg',
-    bio: 'Hi! My name is Oliver and I am a fourth-year undergraduate studying mathematics. I love all kinds of sports. I am involved in the college football teams, netball team, squash club, rugby team and cricket team. I keep everyone up to date with sports in college and the community. Reach out with any sports questions or ideas!',
+    image: 'https://static.wixstatic.com/media/78866e_0b461e601b864ea6b9e02c82d1b99509~mv2.png/v1/crop/x_139,y_0,w_1211,h_1597/fill/w_546,h_720,al_c,q_90,enc_avif,quality_auto/empty_edited.png',
+    bio: 'tbd',
     category: 'operations'
   }
 ];
